8e19fd0cfdb2296dfdde276197d0cb6905b00d50
ShieldsJohn <>
2023-10-31 19:54:40 +0000

    Initial JS structure (functions)

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//begins with player entering name and pressing play and play button being made hidden
+function runGame() {
+
+}
+//either true or false
+function checkAnswer() {
+
+}
+//increment to 10, displaying correct answers as green and incorrect as red in the score-bar
+function updateScore() {
+
+}
+//once question answered and score updated, the next question will load
+function runNextQuestion() {
+
+}
+//once all ten questions have been answered the player is told if won or lost and invited to play again
+function endGame() {
+
+}
