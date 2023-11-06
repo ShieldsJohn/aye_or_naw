7da050e51dac2651ac039c5f92f9f12da6561e45
ShieldsJohn <>
2023-11-06 19:54:09 +0000

    Rework q&a array

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,73 +15,43 @@ document.addEventListener("DOMContentLoaded", function() {
 //question and answer array
 let questionsAnswers = [
     {question: "Dundee was a former capital of Scotland - Aye or Naw?", 
-        answers: [
-            {option:"Aye", answer: false},
-            {option:"Naw", answer: true},
-        ]
+        correctAnswer: Naw,
     },
 
     {question: "Gorillas usually eat more than 15kg of food per day - Aye or Naw?",
-        answers: [
-            {option:"Aye", answer: true},
-            {option:"Naw", answer: false},
-        ]
+        correctAanswer: Aye,
     },
 
     {question: "K2 is the second highest mountain in the world - Aye or Naw?",
-        answers: [
-            {option:"Aye", answer: true},
-            {option:"Naw", answer: false},
-        ]
+        correctAnswer: Aye,
     },
 
     {question: "Irn-Bru was first created in Edinburgh - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: false},
-        {option:"Naw", answer: true},
-    ]
+        correctAnswer: Naw,
     },
 
     {question: "The song ‘(Everything I Do) I Do It For You’ by Bryan Adams is the UK’s longest running number one single - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: true},
-        {option:"Naw", answer: false},
-        ]
+        correctAnswer: Aye,
     },
 
     {question: "North America is the fourth largest continent in the world - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: false},
-        {option:"Naw", answer: true},
-        ]
+        correctAnswer: Naw,
     },
 
     {question: "Sir Chris Hoy has won six Olympic gold medals in track cylcing - Ayr or Naw?",
-    answers: [
-        {option:"Aye", answer: true},
-        {option:"Naw", answer: false},
-        ]
+        correctAnswer: Aye,
     },
 
     {question: "PDF stands for Personal Document Format - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: false},
-        {option:"Naw", answer: true},
-        ]
+        correctAnswer: Naw,
     },
 
     {question: "The London Underground has more stations than the Paris Metro - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: false},
-        {option:"Naw", answer: true},
-        ]
+        correctAnswer: Naw,
     },
 
     {question: "The planet Pluto is smaller than the moon - Aye or Naw?",
-    answers: [
-        {option:"Aye", answer: true},
-        {option:"Naw", answer: false},
-        ]
+        correctAnswer: Aye,
     },
  ];
 
@@ -101,19 +71,23 @@ plrName.addEventListener("input", () => {
 
 //load question upon entering valid player name
     let q1 = 0;
+    let score = document.getElementById("score1");
     let aye = document.getElementById("aye-button")
     let naw = document.getElementById("naw-button")
 
     document.getElementById("game-area").style.display = "none";
-    document.addEventListener("input", function () {
+    document.addEventListener("input", function() {
         if (plrName.value.length === 3) {
             document.getElementById("game-area").style.display = "block";
         }
     })
     document.getElementById("question-box").innerHTML = questionsAnswers[q1].question;
     aye.innerHTML = questionsAnswers[q1].answers[0].option;
-   
-}
+    aye.onclick = () => {
+
+                }
+    }
+
   
 //increment to 10, displaying correct answers as green and incorrect as red in the score-bar
 function updateScore() {
