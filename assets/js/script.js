960cc6f27a43a39b1e8167af29081937f651c738
ShieldsJohn <>
2023-11-06 21:39:24 +0000

    Reinstate first question loading in question-box

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,48 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
     runGame()
 })
 
-//question and answer array
-let questionsAnswers = [
-    {question: "Dundee was a former capital of Scotland - Aye or Naw?", 
-        correctAnswer: Naw,
-    },
-
-    {question: "Gorillas usually eat more than 15kg of food per day - Aye or Naw?",
-        correctAanswer: Aye,
-    },
-
-    {question: "K2 is the second highest mountain in the world - Aye or Naw?",
-        correctAnswer: Aye,
-    },
-
-    {question: "Irn-Bru was first created in Edinburgh - Aye or Naw?",
-        correctAnswer: Naw,
-    },
-
-    {question: "The song ‘(Everything I Do) I Do It For You’ by Bryan Adams is the UK’s longest running number one single - Aye or Naw?",
-        correctAnswer: Aye,
-    },
-
-    {question: "North America is the fourth largest continent in the world - Aye or Naw?",
-        correctAnswer: Naw,
-    },
-
-    {question: "Sir Chris Hoy has won six Olympic gold medals in track cylcing - Ayr or Naw?",
-        correctAnswer: Aye,
-    },
-
-    {question: "PDF stands for Personal Document Format - Aye or Naw?",
-        correctAnswer: Naw,
-    },
-
-    {question: "The London Underground has more stations than the Paris Metro - Aye or Naw?",
-        correctAnswer: Naw,
-    },
-
-    {question: "The planet Pluto is smaller than the moon - Aye or Naw?",
-        correctAnswer: Aye,
-    },
- ];
 
 
 function runGame() {
@@ -72,8 +30,8 @@ plrName.addEventListener("input", () => {
 //load question upon entering valid player name
     let q1 = 0;
     let score = document.getElementById("score1");
-    let aye = document.getElementById("aye-button")
-    let naw = document.getElementById("naw-button")
+    const aye = document.getElementById("aye-button");
+    const naw = document.getElementById("naw-button");
 
     document.getElementById("game-area").style.display = "none";
     document.addEventListener("input", function() {
@@ -81,12 +39,66 @@ plrName.addEventListener("input", () => {
             document.getElementById("game-area").style.display = "block";
         }
     })
-    document.getElementById("question-box").innerHTML = questionsAnswers[q1].question;
-    aye.innerHTML = questionsAnswers[q1].answers[0].option;
-    aye.onclick = () => {
+    //question and answer array
+const questionsAnswers = [
+    {question: "Dundee was a former capital of Scotland - Aye or Naw?", 
+        correctAnswer: naw,
+    },
 
-                }
+    {question: "Gorillas usually eat more than 15kg of food per day - Aye or Naw?",
+        correctAnswer: aye,
+    },
+
+    {question: "K2 is the second highest mountain in the world - Aye or Naw?",
+        correctAnswer: aye,
+    },
+
+    {question: "Irn-Bru was first created in Edinburgh - Aye or Naw?",
+        correctAnswer: naw,
+    },
+
+    {question: "The song ‘(Everything I Do) I Do It For You’ by Bryan Adams is the UK’s longest running number one single - Aye or Naw?",
+        correctAnswer: aye,
+    },
+
+    {question: "North America is the fourth largest continent in the world - Aye or Naw?",
+        correctAnswer: naw,
+    },
+
+    {question: "Sir Chris Hoy has won six Olympic gold medals in track cylcing - Ayr or Naw?",
+        correctAnswer: aye,
+    },
+
+    {question: "PDF stands for Personal Document Format - Aye or Naw?",
+        correctAnswer: naw,
+    },
+
+    {question: "The London Underground has more stations than the Paris Metro - Aye or Naw?",
+        correctAnswer: naw,
+    },
+
+    {question: "The planet Pluto is smaller than the moon - Aye or Naw?",
+        correctAnswer: aye,
+    },
+ ];
+
+    document.getElementById("question-box").innerHTML = questionsAnswers[q1].question;
+    
     }
+
+//connect buttons with answers and run scoring
+function clickAnswers() {
+    const aye = document.getElementById("aye-button");
+    const naw = document.getElementById("naw-button");
+
+    document.addEventListener("click", aye)
+        alert("Aye, clicked")
+    document.addEventListener("click", naw)
+        alert("Naw, clicked");
+}
+    
+                
+    
 
   
 //increment to 10, displaying correct answers as green and incorrect as red in the score-bar
